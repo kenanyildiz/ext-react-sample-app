3f519c88e98f94838560ae7956d844a75890e24c
Kenan Yıldız <kenan.yildiz@solvoyo.com>
2021-02-22 16:58:23 +0300

    Removed unnecessary file.

--- src/Hello.js
+++ src/Hello.js
@@ -1,15 +1,13 @@
-import { Panel } from '@sencha/ext-react-classic'
 
 /*
 * Here: We request to import our custom Hello package which in ext-react/packages/hello
 * On the older version, we import it in this way => import { HelloWorld } from '@extjs/reactor/classic'
-*
 * */
 
 function Hello() {
   return (
     <div className="Hello">
-      {/*<Panel html={'HelloWorldView'}></Panel>*/}
+      Hello
       {/* HelloWorld ExtJS component should place here */}
     </div>
   );
